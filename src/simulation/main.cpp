dbb429ef8405c902843c306935406e1f12a5cb0e
timowaibel <timowaibel@users.noreply.github.com>
2024-02-09 01:13:43 +0100

    added opengl to simulation

--- src/simulation/main.cpp
+++ src/simulation/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <GL/glut.h>
 
 #include "lidarSensorSim.h"
 #include "vehicleActuatorSim.h"
 #include "selfdrivingVehicle.h"
 
-int main()
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_POLYGON);
+    glColor3f(1, 0, 0);
+    glVertex3f(-0.6, -0.75, 0.5);
+    glColor3f(0, 1, 0);
+    glVertex3f(0.6, -0.75, 0);
+    glColor3f(0, 0, 1);
+    glVertex3f(0, 0.75, 0);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char **argv)
 {
     std::cout << "Test" << std::endl;
     LidarSensorSim lidarSensorSim = LidarSensorSim();
@@ -17,4 +34,15 @@ int main()
     VehicleActuatorSim vehicleActuatorSim = VehicleActuatorSim();
 
     SelfdrivingVehicle selfdrivingVehicle = SelfdrivingVehicle(&lidarSensorSim, &vehicleActuatorSim);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    glutInitWindowPosition(80, 80);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("Self-driving Car Simulation");
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
 }
