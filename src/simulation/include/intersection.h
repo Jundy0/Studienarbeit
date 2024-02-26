510dfbc9eb4106ee06c5d3e59bc8259a5a50087f
Timo Waibel <61755965+timowaibel@users.noreply.github.com>
2024-02-26 07:31:23 +0000

    Fixes and comments

--- src/simulation/include/intersection.h
+++ src/simulation/include/intersection.h
@@ -7,8 +7,30 @@
 
 #include "obstacle.h"
 
-bool intersectsObstacles(const sf::Vector2f& rayOrigin, float rayAngle, const std::vector<Obstacle>& obstacles, sf::FloatRect& windowRect, sf::Vector2f& intersectionPoint);
-bool intersectsRect(const sf::Vector2f& rayOrigin, const sf::Vector2f& rayDirection, const sf::FloatRect& rect, , sf::Vector2f& intersectionPoint);
-bool intersects(const sf::Vector2f& rayOrigin, const sf::Vector2f& rayDirection, const sf::Vector2f& p1, const sf::Vector2f& p2, sf::Vector2f& intersectionPoint);
+/// @brief Gets the intersections of a Ray with the Obstacles and the Bounds of the Window.
+/// @param rayOrigin The Origin of the Ray. 
+/// @param rayAngle The Angle of the Ray in Radiant.
+/// @param obstacles A List of Obstacles.
+/// @param windowRect The Bounds of the Window as a Reactangle.
+/// @param intersectionPoints A List of Points where the Ray intersected with an Obstacle or the Bounds of the Window. 
+/// @return True, if an Intersection was found.
+bool intersectsObstacles(const sf::Vector2f &rayOrigin, float rayAngle, const std::vector<Obstacle> &obstacles, sf::FloatRect &windowRect, std::vector<sf::Vector2f> &intersectionPoints);
+
+/// @brief Gets the intersections of a Ray with a Reactangle.
+/// @param rayOrigin The Origin of the Ray.
+/// @param rayDirection The Direction of the Ray.
+/// @param rect The Reactangle to check if the Ray intersects.
+/// @param intersectionPoints A List of Points of Intersections, where the new Intersection should be appended.
+/// @return True, if an Intersection was found.
+bool intersectsRect(const sf::Vector2f &rayOrigin, const sf::Vector2f &rayDirection, const sf::FloatRect &rect, std::vector<sf::Vector2f> &intersectionPoints);
+
+/// @brief Gets the intersections of a Ray with straight Line between two Points.
+/// @param rayOrigin The Origin of the Ray.
+/// @param rayDirection The Direction of the Ray.
+/// @param p1 The start Point of the straight Line.
+/// @param p2 The end Point of the straight Line.
+/// @param intersectionPoints A List of Points of Intersections, where the new Intersection should be appended.
+/// @return True, if an Intersection was found.
+bool intersects(const sf::Vector2f &rayOrigin, const sf::Vector2f &rayDirection, const sf::Vector2f &p1, const sf::Vector2f &p2, std::vector<sf::Vector2f> &intersectionPoints);
 
 #endif // __INTERSECTION_H__
--- src/simulation/src/intersection.cpp
+++ src/simulation/src/intersection.cpp
@@ -2,13 +2,13 @@
 
 #include <cmath>
 
-bool intersectsObstacles(const sf::Vector2f& rayOrigin, float rayAngle, const std::vector<Obstacle>& obstacles, sf::FloatRect& windowRect, std::vector<sf::Vector2f>& intersectionPoints)
+bool intersectsObstacles(const sf::Vector2f &rayOrigin, float rayAngle, std::vector<Obstacle> &obstacles, sf::FloatRect &windowRect, std::vector<sf::Vector2f> &intersectionPoints)
 {
     sf::Vector2f rayDirection(std::cos(rayAngle), std::sin(rayAngle));
 
-    for (auto &obstacle : this->obstacles) 
+    for (auto &obstacle : obstacles)
     {
-        intersectsRect(rayOrigin, rayDirection, obstacle.getPosition(), intersectionPoints)
+        intersectsRect(rayOrigin, rayDirection, obstacle.getPosition(), intersectionPoints);
     }
 
     intersectsRect(rayOrigin, rayDirection, windowRect, intersectionPoints);
@@ -18,7 +18,7 @@ bool intersectsObstacles(const sf::Vector2f& rayOrigin, float rayAngle, const st
     return true;
 }
 
-bool intersectsRect(const sf::Vector2f& rayOrigin, const sf::Vector2f& rayDirection, const sf::FloatRect& rect, std::vector<sf::Vector2f>& intersectionPoints)
+bool intersectsRect(const sf::Vector2f &rayOrigin, const sf::Vector2f &rayDirection, const sf::FloatRect &rect, std::vector<sf::Vector2f> &intersectionPoints)
 {
     // Calculate intersection points with the rectangle's edges
     sf::Vector2f p1(rect.left, rect.top);
@@ -30,7 +30,7 @@ bool intersectsRect(const sf::Vector2f& rayOrigin, const sf::Vector2f& rayDirect
     if (intersects(rayOrigin, rayDirection, p1, p2, intersectionPoints) ||
         intersects(rayOrigin, rayDirection, p2, p3, intersectionPoints) ||
         intersects(rayOrigin, rayDirection, p3, p4, intersectionPoints) ||
-        intersects(rayOrigin, rayDirection, p4, p1, intersectionPoints)) 
+        intersects(rayOrigin, rayDirection, p4, p1, intersectionPoints))
     {
         return true;
     }
@@ -38,7 +38,7 @@ bool intersectsRect(const sf::Vector2f& rayOrigin, const sf::Vector2f& rayDirect
     return false;
 }
 
-bool intersects(const sf::Vector2f& rayOrigin, const sf::Vector2f& rayDirection, const sf::Vector2f& p1, const sf::Vector2f& p2, std::vector<sf::Vector2f>& intersectionPoints)
+bool intersects(const sf::Vector2f &rayOrigin, const sf::Vector2f &rayDirection, const sf::Vector2f &p1, const sf::Vector2f &p2, std::vector<sf::Vector2f> &intersectionPoints)
 {
     sf::Vector2f v1 = p1 - rayOrigin;
     sf::Vector2f v2 = p2 - rayOrigin;
@@ -48,13 +48,15 @@ bool intersects(const sf::Vector2f& rayOrigin, const sf::Vector2f& rayDirection,
     float cross2 = v2.x * rayDirection.y - v2.y * rayDirection.x;
 
     // If the signs are different, it means the points are on different sides of the ray
-    if ((cross < 0 && cross2 > 0) || (cross > 0 && cross2 < 0)) 
+    if ((cross < 0 && cross2 > 0) || (cross > 0 && cross2 < 0))
     {
         // Calculate the intersection point using linear interpolation
         float t = cross / (cross - cross2);
-        const sf::Vector2f intersectionPoint();
+        
+        sf::Vector2f intersectionPoint = sf::Vector2f();
         intersectionPoint = rayOrigin + t * rayDirection;
         intersectionPoints.push_back(intersectionPoint);
+
         return true;
     }
 
