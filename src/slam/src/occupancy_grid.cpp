Jundy0/Studienarbeit

commit 704c4b81bb58ba21f5349c70ac7f1d04a3730b65

Resetcolor to default after printing grid

src/slam/src/occupancy_grid.cpp
@@ -44,6 +44,7 @@ void OccupancyGrid::visualize() {
     const string BLACK_COLOR = "\033[40m";
     const string GREEN_COLOR = "\033[42m";
     const string WHITE_COLOR = "\033[47m";
+    const string DEFAULT_COLOR = "\033[0m";
 
     cout << WHITE_COLOR;
 
@@ -56,7 +57,7 @@ void OccupancyGrid::visualize() {
             else
                 cout << " ";
         }
-        cout << endl;
+        cout << DEFAULT_COLOR << endl;
     }
 }
 

commit a56c344d650a223a62c42d07266ec63cea8e7ae6

Fix coloring bug introduced with merge 704c4b8
See pull request: https://github.com/Jundy0/Studienarbeit/pull/1

src/slam/src/occupancy_grid.cpp
@@ -57,8 +57,10 @@ void OccupancyGrid::visualize() {
             else
                 cout << " ";
         }
-        cout << DEFAULT_COLOR << endl;
+        cout << endl;
     }
+
+    cout << DEFAULT_COLOR << endl;
 }
 
 vector< vector<double> > OccupancyGrid::getDataFromFile(string filePath) {