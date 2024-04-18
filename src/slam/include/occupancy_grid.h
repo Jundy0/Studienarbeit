cacb4d200320be4c4594864aed45d58e011143b0
Jundy0 <101015472+Jundy0@users.noreply.github.com>
2024-04-18 17:07:31 +0200

    Merged ICP Code to Slam-Test

--- src/slam/include/occupancy_grid.h
+++ src/slam/include/occupancy_grid.h
@@ -3,27 +3,28 @@
 #include <fstream>
 #include <string>
 
-#include <../lib/eigen/Eigen/Dense>
+#include "../lib/eigen/Eigen/Dense"
 
 using namespace std;
 
-class OccupancyGrid {
-    
-    public:
-        OccupancyGrid();
-        void updateProbMap(string filePath);
-        void visualize();
+class OccupancyGrid
+{
 
-    private:
-        double probOcc;
-        double probFree;
-        int gridWidth;
-        int gridHeight;
+public:
+    OccupancyGrid();
+    void updateProbMap(string filePath);
+    void visualize();
 
-        Eigen::MatrixXd probMap;
+private:
+    double probOcc;
+    double probFree;
+    int gridWidth;
+    int gridHeight;
 
-        Eigen::MatrixX2d OccupancyGrid::getDataFromFile(string filePath);
-        pair<Eigen::MatrixX2i, Eigen::MatrixX2i> OccupancyGrid::getPoints(string filePath);
-        Eigen::MatrixX2i OccupancyGrid::bresenham(int robPosX, int robPosY, int x, int y);
-        Eigen::RowVector2i OccupancyGrid::polarToCartesian(Eigen::RowVector2d polarPoint, Eigen::RowVector2i robPos);
+    Eigen::MatrixXd probMap;
+
+    Eigen::MatrixX2d getDataFromFile(string filePath);
+    pair<Eigen::MatrixX2i, Eigen::MatrixX2i> getPoints(string filePath);
+    Eigen::MatrixX2i bresenham(int robPosX, int robPosY, int x, int y);
+    Eigen::RowVector2i polarToCartesian(Eigen::RowVector2d polarPoint, Eigen::RowVector2i robPos);
 };
--- src/slam/main.cpp
+++ src/slam/main.cpp
@@ -1,12 +1,16 @@
-#include "include/occupancy_grid.h"
+// #include "include/occupancy_grid.h"
+#include "include/icp_handler.h"
 
 using namespace std;
 
 int main()
 {
-    OccupancyGrid grid;
-    grid.updateProbMap("./log.csv");
-    grid.visualize();
-    
+    // OccupancyGrid grid;
+    // grid.updateProbMap("./log.csv");
+    // grid.visualize();
+
+    IcpHandler icp;
+    icp.dummy_icp();
+
     return 0;
 }
