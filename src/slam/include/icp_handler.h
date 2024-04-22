c156ab9aa33dd7da8fcba26bd11396770ff2855a
Jundy0 <101015472+Jundy0@users.noreply.github.com>
2024-04-22 12:16:49 +0200

    fixed merge

--- src/slam/include/icp_handler.h
+++ src/slam/include/icp_handler.h
@@ -8,15 +8,19 @@
 
 struct TransformationComponents
 {
-    Eigen::Vector3d translation_vector;
-    double rotation_angle_deg; // Rotation angle in degrees
+    Eigen::RowVector2i translation_vector;
+    double rotation_angle; // Rotation angle in degrees
 };
 
 class IcpHandler
 {
 public:
     IcpHandler();
-    TransformationComponents extractTransformation(const Eigen::Matrix4d &);
-    TransformationComponents call_icp(Eigen::MatrixXd, Eigen::MatrixXd);
+    TransformationComponents call_icp(Eigen::MatrixX2d, Eigen::MatrixX2d);
 
+private:
+    TransformationComponents execute_icp(Eigen::MatrixXd , Eigen::MatrixXd );
+    Eigen::MatrixXd get_matrix_from_points(Eigen::MatrixX2d);
+    Eigen::MatrixXd polar_to_cartesian_from_matrix(Eigen::MatrixX2d);
+    TransformationComponents extractTransformation(const Eigen::Matrix4d &);
 };
--- src/slam/src/icp_handler.cpp
+++ src/slam/src/icp_handler.cpp
@@ -60,70 +60,9 @@ TransformationComponents IcpHandler::execute_icp(Eigen::MatrixXd initial_matrix,
     return trans_comps;
 }
 
-TransformationComponents IcpHandler::call_icp(Eigen::Matrix2d scan_one, Eigen::Matrix2d scan_two)
+TransformationComponents IcpHandler::call_icp(Eigen::MatrixX2d scan_one, Eigen::MatrixX2d scan_two)
 {
     Eigen::MatrixXd initial_mattrix = get_matrix_from_points(scan_one);
     Eigen::MatrixXd transformed_matirx = get_matrix_from_points(scan_two);
     return execute_icp(initial_mattrix,transformed_matirx);
-}
-
-Eigen::MatrixXd IcpHandler::translate_rotate_matrix(const Eigen::MatrixXd &cartesian_coords, const std::vector<double> &translation_vector, double rotation_angle)
-{
-    // Convert rotation angle to radians
-    double theta = rotation_angle * M_PI / 180.0;
-    double cos_theta = cos(theta);
-    double sin_theta = sin(theta);
-
-    // Define rotation matrix
-    Eigen::Matrix3d rotation_matrix;
-    rotation_matrix << cos_theta, -sin_theta, 0,
-        sin_theta, cos_theta, 0,
-        0, 0, 1;
-
-    // Create a copy of the input matrix for manipulation
-    Eigen::MatrixXd rotated_translated_matrix = cartesian_coords;
-
-    // Apply rotation to each point
-    rotated_translated_matrix = rotated_translated_matrix * rotation_matrix;
-
-    // Apply translation matrix to the points
-    for (int i = 0; i < rotated_translated_matrix.rows(); i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            rotated_translated_matrix.row(i)[j] += translation_vector[j];
-        }
-    }
-
-    return rotated_translated_matrix;
-}
-
-Eigen::MatrixXd IcpHandler::apply_transformation(Eigen::MatrixXd start_matrix)
-{
-    std::vector<double> translation_vector = {4.0, 4.0, 0.0}; // Example translation
-    double rotation_angle = 360.0;                             // Example rotation angle
-    return translate_rotate_matrix(start_matrix, translation_vector, rotation_angle);
-}
-
-Eigen::MatrixXd IcpHandler::get_matrix_from_points(Eigen::MatrixX2d points = load_points_from_file())
-{
-    return polar_to_cartesian_from_matrix(points);
-}
-
-void IcpHandler::dummy_icp()
-{
-    // Convert polar coordinates to Cartesian coordinates
-    Eigen::MatrixXd initial_coordinates = get_matrix_from_points();
-    // Perform translation and rotation
-    Eigen::MatrixXd rotated_translated_matrix = apply_transformation(initial_coordinates);
-
-    TransformationComponents trans_comps = execute_icp(initial_coordinates, rotated_translated_matrix);
-
-    std::cout << trans_comps.translation_vector << std::endl;
-    std::cout << trans_comps.rotation_angle << std::endl;
-};
-
-Eigen::MatrixX2d load_points_from_file()
-{
-    return getFileData();
 }
